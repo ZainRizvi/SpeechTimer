b4245eb0d1ea86365ee06ff3988855c67c894131
ZainRizvi <zhinker@gmail.com>
2014-02-18 02:09:20 -0800

    Initial Checkin - Crude timer added

---
+++ SpeechTimer/Controllers/TimerController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SpeechTimer.Controllers
+{
+    public class TimerController : Controller
+    {
+        //
+        // GET: /Timer/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
---
+++ SpeechTimer/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SpeechTimer.MvcApplication" Language="C#" %>
