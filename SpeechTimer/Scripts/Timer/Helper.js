0979d12f1c25f22f2bb173b34e91e4513b105556
ZainRizvi <zhinker@gmail.com>
2014-02-19 00:49:16 -0800

    Did I change something or just miss this in the last submit?

---
+++ SpeechTimer/Scripts/Timer/Helper.js
@@ -0,0 +1,12 @@
+﻿
+function toDoubleDigits(number) {
+    if (number < 10) {
+        return "0" + number;
+    } else {
+        return number;
+    }
+}
+
+function toTimeSpan(hours, minutes, seconds) {
+    return toDoubleDigits(hours) + ":" + toDoubleDigits(minutes) + ":" + toDoubleDigits(seconds)
+}
